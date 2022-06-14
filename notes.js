128bcd70b7de8142375c63d42a6650cd351e0968
parthrastogi15 <88496008+parthrastogi15@users.noreply.github.com>
2022-06-14 23:28:21 +0530

    Initial commit

---
+++ notes.js
@@ -0,0 +1,55 @@
+const chalk = require("chalk");
+const fs = require("fs");
+
+const getNotes = () => "Your notes...";
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  debugger;
+
+  if (!duplicateNotes.length) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken!"));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed!"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No note found!"));
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+};
